acc83553d8de8ff518dc6098913fa41225041d50
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-07-29 15:09:38 -0300

    Updates

---
+++ precache-manifest.b5d93a068df0d1697edf2a4bdf706c4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce9c45cfa54c20ad540ad74fa9081e5",
+    "url": "/movie-cards-library-stateful/index.html"
+  },
+  {
+    "revision": "682ec80292b520471b63",
+    "url": "/movie-cards-library-stateful/static/css/main.bf61a593.chunk.css"
+  },
+  {
+    "revision": "bfb89918fe433bfa9d54",
+    "url": "/movie-cards-library-stateful/static/js/2.1163f794.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movie-cards-library-stateful/static/js/2.1163f794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "682ec80292b520471b63",
+    "url": "/movie-cards-library-stateful/static/js/main.c7d5a4f9.chunk.js"
+  },
+  {
+    "revision": "bdf93eebb3ecba7817db",
+    "url": "/movie-cards-library-stateful/static/js/runtime-main.2979b9ff.js"
+  }
+]);
